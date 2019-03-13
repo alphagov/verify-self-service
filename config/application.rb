4f6e4dd34fca70715de09f5d9c5cf255d5a54085
Jamie Maynard <james.maynard@digital.cabinet-office.gov.uk>
2019-03-13 16:16:37 +0000

    Removed cookie-store config from application.rb

--- config/application.rb
+++ config/application.rb
@@ -29,9 +29,5 @@ module VerifySelfService
     # Don't generate system test files.
     config.generators.system_tests = nil
 
-    #Configure cookie store for middlewear
-    config.session_store :cookie_store, key: '_interslice_session'
-    config.middleware.use ActionDispatch::Cookies # Required for all session management
-    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
   end
 end
