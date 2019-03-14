8a8f39b0b0c163ae46743ead56107787a0b0bf18
Jamie Maynard <james.maynard@digital.cabinet-office.gov.uk>
2019-03-14 18:03:29 +0000

    Followed Chris's suggestions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,13 +7,13 @@ module ApplicationHelper
   def logout_url
     if session[:provider] == 'cognito-idp'
         host = "#{request.protocol}#{request.host}:#{request.port}" || "http://localhost:3000"
-        URI.join(
-            Rails.application.secrets.cognito_user_pool_site,
-            "logout?"+
-            {
+        query_params = {
                 "client_id": Rails.application.secrets.cognito_client_id,
                 "logout_uri": "#{host}#{logout_callback_path}"
-            }.to_query).to_s
+            }.to_query
+        URI.join(
+            Rails.application.secrets.cognito_user_pool_site,
+            "logout?#{query_params}").to_s
     else
         logout_callback_path
     end
