0263e9da8b85e51e1c8ba19ba08f2972df744713
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2019-03-20 17:21:34 +0000

    XerJ40vr: remove home artifacts

--- spec/acceptance/visit_home_controller_acceptance_spec.rb
+++ spec/acceptance/visit_home_controller_acceptance_spec.rb
@@ -3,12 +3,10 @@ require 'acceptance_helper'
 RSpec.describe 'Homepage', type: :feature, acceptance: true do
 
   it 'shows greeting with JS', js: true do
-    visit "https://#{ENV["TEST_DOMAIN"]}/home/index"
+    visit "https://#{ENV["TEST_DOMAIN"]}"
     fill_in('username', :with => 'test@test.test')
     fill_in('password', :with => 'Password!1')
     find("[name='signInSubmitButton']").click
-    expect(page).to have_content 'Home#index'
-    expect(page).to have_content 'Pipeline Test 1'
+    expect(page).to have_content 'Components#index'
   end
-
 end
