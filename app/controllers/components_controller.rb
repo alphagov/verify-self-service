ca587f3a8845fb65e543ad489a9d05cb3649099d
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2019-03-20 16:25:56 +0000

    XerJ40vr: fixed indentation

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,34 +1,33 @@
 class ComponentsController < ApplicationController
-    def index
-      @components = Component.all
-    end
-
-    def new
-      @component = NewComponentEvent.new
-    end
-
-    def show
-      @component = Component.find(params[:id])
-      @component_certificates = @component.certificates
-    end
-
-    def create
-      @component = NewComponentEvent.create(component_params)
-      if @component.valid?
-        redirect_to components_path
-      else
-        Rails.logger.info(@component.errors.full_messages)
-        render :new
-      end
-    end
-
-    private
-
-    def component_params
-      params.require(:component).permit(
-        :name,
-        :component_type
-      )
-    end
-
+  def index
+    @components = Component.all
   end
+
+  def new
+    @component = NewComponentEvent.new
+  end
+
+  def show
+    @component = Component.find(params[:id])
+    @component_certificates = @component.certificates
+  end
+
+  def create
+    @component = NewComponentEvent.create(component_params)
+    if @component.valid?
+      redirect_to components_path
+    else
+      Rails.logger.info(@component.errors.full_messages)
+      render :new
+    end
+  end
+
+private
+
+  def component_params
+    params.require(:component).permit(
+      :name,
+      :component_type
+    )
+  end
+end
