b4e4df5c54d990733cc0eacd171d0317d75a47ae
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2019-03-20 11:35:06 +0000

    XerJ40vr: removed home_controller_spec

--- spec/controllers/components_controller_spec.rb
+++ spec/controllers/components_controller_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 require 'auth_test_helper'
 
-RSpec.describe CertificatesController, type: :controller do
+RSpec.describe ComponentsController, type: :controller do
   before(:each) do
     populate_session
   end
-
   describe "GET #index" do
     it "returns http success" do
       get :index
