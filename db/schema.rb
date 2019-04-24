dc395460b963b7d0bd9dddce6643801d9ee73d84
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2019-04-24 16:09:12 +0100

    ee2PV5rg: Add enabled boolean to certificates table
    Co-authored-by: Kerr Rainey <kerr.rainey@digital.cabinet-office.gov.uk>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_203341) do
+ActiveRecord::Schema.define(version: 2019_04_24_150803) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_04_10_203341) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "component_id"
+    t.boolean "enabled", default: true
     t.index ["component_id"], name: "index_certificates_on_component_id"
   end
 
