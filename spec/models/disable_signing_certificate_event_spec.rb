a3c14b4ec403fd50208c508ef06afe7476577957
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2019-04-30 16:46:40 +0100

    eOx5BZ3C: Add DisableSigningCertificate event
    - Certificate is disabled when this event is created.
    
    Co-authored-by: Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>

---
+++ spec/models/disable_signing_certificate_event_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe DisableSigningCertificateEvent, type: :model do
+  include CertificateSupport
+  root = PKI.new
+  good_cert_value = root.generate_encoded_cert(expires_in: 2.months)
+  expired_cert_value = root.generate_encoded_cert(expires_in: -2.months)
+  component_params = { component_type: 'SP', name: 'Test Service Provider' }
+  component = NewComponentEvent.create(component_params).component
+
+  let(:signing_certificate) do
+    UploadCertificateEvent.create(
+        usage: 'signing', value: good_cert_value, component_id: component.id
+    ).certificate
+  end
+
+  let(:expired_signing_certificate) do
+    UploadCertificateEvent.create(
+        usage: 'signing', value: expired_cert_value, component_id: component.id
+    ).certificate
+  end
+
+  let(:encryption_certificate) do
+    UploadCertificateEvent.create(
+        usage: 'encryption', value: good_cert_value, component_id: component.id
+    ).certificate
+  end
+
+  def disable_signing_certificate_event
+    DisableSigningCertificateEvent.create(certificate: signing_certificate)
+  end
+
+
+  it 'disables a signing certificate' do
+    cert = disable_signing_certificate_event.certificate
+    expect(cert.enabled).to eq(false)
+  end
+
+  it 'must be persisted' do
+    event = disable_signing_certificate_event
+    expect(event).to be_valid
+    expect(event).to be_persisted
+  end
+
+  it 'cannot be created with expired certificate' do
+    event = DisableSigningCertificateEvent.create(
+        certificate: expired_signing_certificate
+    )
+    expect(event.certificate).not_to be_valid
+    expect(event).not_to be_persisted
+  end
+
+  it 'must be signing' do
+    event = DisableSigningCertificateEvent.create(
+        certificate: encryption_certificate
+    )
+    cert = event.certificate
+    expect(cert.usage).to eq('encryption')
+    expect(event).not_to be_persisted
+  end
+end
