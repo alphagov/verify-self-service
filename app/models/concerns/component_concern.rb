5c07f9c50279527bbc0b3d4ed1786420f1d3ffcb
brenetic <b@brenetic.com>
2019-06-10 15:51:24 +0100

    VL3EbxHV: Mini refactor
    Co-authored-by: Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>

--- app/models/concerns/component_concern.rb
+++ app/models/concerns/component_concern.rb
@@ -4,8 +4,4 @@ module ComponentConcern
   def klass_component(name)
     name.safe_constantize
   end
-
-  def view_component_type(vsp)
-    vsp ? CONSTANTS::VSP_SHORT : CONSTANTS::SP_SHORT
-  end
 end
--- app/models/sp_component.rb
+++ app/models/sp_component.rb
@@ -1,5 +1,8 @@
 class SpComponent < Component
   has_many :certificates, as: :component
   has_many :services
-  include ComponentConcern
+
+  def view_component_type(vsp)
+    vsp ? CONSTANTS::VSP_SHORT : CONSTANTS::SP_SHORT
+  end
 end
