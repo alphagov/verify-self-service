9e59199f13f954e2056fc69c96b10a1de1226eaf
Jamie Maynard <james.maynard@digital.cabinet-office.gov.uk>
2019-07-25 14:01:51 +0100

    Fixed bug in event creation
    Bug was when the user_id was nil it would use the current
    user_id in its place.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,13 +1,17 @@
 class Event < ApplicationRecord
   include UserInfo
   after_initialize :default_values
+  before_create :set_user_id
   scope :newest_first, -> { order('created_at DESC') }
 
 private
 
+  def set_user_id
+    self.user_id ||= current_user&.user_id
+  end
+
   def default_values
     self.data ||= {}
-    self.user_id ||= current_user&.user_id
   end
 
   def self.data_attributes(*names) # rubocop:disable  IneffectiveAccessModifier
