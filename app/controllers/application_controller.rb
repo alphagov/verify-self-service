bf5d3b57174076bbbe2ce96ea79177527dbe9cff
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2019-08-01 10:51:50 +0100

    WICk8xMh: Remove support for second langauge
    https://trello.com/c/WICk8xMh/517-use-localization-in-frontend
    
    Only have a default langauage
    PR fixes to make en.yml more reusable
    Rebase with master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,6 @@ class ApplicationController < ActionController::Base
   before_action :set_user
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  before_action :set_locale
-
 protected
 
   def configure_permitted_parameters
@@ -16,11 +14,5 @@ protected
   # Sets the current user into a named Thread location so that it can be accessed by models and observers
   def set_user
     UserInfo.current_user = authenticate_user!
-  end
-
-private
-
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
