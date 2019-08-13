8538d6b966cade63a65379153e070431d9ff15e6
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2019-08-13 17:24:08 +0100

    Migration to remove creation of user table
    - users table snuck its way back in :(

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_110003) do
+ActiveRecord::Schema.define(version: 2019_08_13_161922) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -95,14 +95,6 @@ ActiveRecord::Schema.define(version: 2019_08_12_110003) do
     t.datetime "updated_at", null: false
     t.string "team_alias"
     t.index ["name"], name: "index_teams_on_name", unique: true
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
