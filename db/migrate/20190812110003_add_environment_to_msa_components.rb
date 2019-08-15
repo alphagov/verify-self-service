f871f32501f826b1c702927a287a209652f29fd0
brenetic <b@brenetic.com>
2019-08-15 17:14:52 +0100

    Adjust previous migrations take 2
    Once more unto the breach . ..

--- db/migrate/20190812110003_add_environment_to_msa_components.rb
+++ db/migrate/20190812110003_add_environment_to_msa_components.rb
@@ -1,6 +1,6 @@
 class AddEnvironmentToMsaComponents < ActiveRecord::Migration[5.2]
   def change
     add_column :msa_components, :environment, :string
-    change_column :msa_components, :environment, :string, null: false, default: 'development'
+    change_column :msa_components, :environment, :string, null: true
   end
 end
