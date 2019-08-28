5d098bbf7407e30a2525bc2551852524b27bdc50
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2019-08-28 15:07:22 +0100

    BAU: Show when certificate are expiring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def format_date_time(cert_date_time)
     cert_date_time.strftime("%e-%m-%Y %H:%M")
   end
+
+  def certificate_expiry(certificate)
+    if certificate.not_after - Time.now < 30.day
+      (certificate.not_after.to_date - Time.now.to_date).to_i.to_s
+    end
+  end
 end
