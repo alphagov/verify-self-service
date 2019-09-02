2b6bb9c97a9c8a6520256a5c3aef2ef939c471df
brenetic <b@brenetic.com>
2019-09-02 11:05:50 +0100

    Adjust Home Page acceptance test
    This is the temporary first step to having a full acceptance test suite.

--- spec/acceptance/visit_home_page_acceptance_spec.rb
+++ spec/acceptance/visit_home_page_acceptance_spec.rb
@@ -1,11 +1,10 @@
+require 'rails_helper'
 require 'acceptance_helper'
 
 RSpec.describe 'Homepage', type: :feature, acceptance: true do
-  it 'shows greeting with JS', js: true do
-    visit "https://#{ENV['TEST_DOMAIN']}"
-    fill_in('user_email', with: 'test@test.test')
-    fill_in('user_password', with: 'Password!1')
-    find("[name='commit']").click
-    expect(page).to have_content 'Manage certificates'
+  it 'visits the service home page successfully', js: true do
+    visit ENV['TEST_DOMAIN']
+
+    expect(page).to have_content t('layout.application.service_name')
   end
 end
