79408c30b01ed3d8578094a8dccafa26d4b8c1c6
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2019-09-04 13:56:44 +0100

    MdAKXfZX: Create tests to show only two signing certificate and the information
    Created tests to show that the button no longer exists to be able to upload more signing certificates if two already exist
    They also test to see if the primary and secondary information is being shown

--- spec/system/visit_index_page_spec.rb
+++ spec/system/visit_index_page_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe 'IndexPage', type: :system do
   before(:each) do
     login_gds_user
   end
+
+  let(:second_certificate) { PKI.new.generate_encoded_cert(expires_in: 9.months) }
+
   it 'shows greeting without JS' do
     visit '/'
     expect(page).to have_content 'Manage certificates'
@@ -13,4 +16,18 @@ RSpec.describe 'IndexPage', type: :system do
     visit '/'
     expect(page).to have_content 'Manage certificates'
   end
+
+  it 'shows whether certificate is primary or secondary when there are two signing certificates' do
+    first_certificate = create(:msa_signing_certificate)
+    defaults = {
+      usage: CERTIFICATE_USAGE::SIGNING,
+      value: second_certificate,
+      component: first_certificate.component
+    }
+    Certificate.create(defaults)
+    visit root_path
+    expect(page).to have_content 'Signing certificate (primary)'
+    expect(page).to have_content 'Signing certificate (secondary)'
+  end
+
 end
--- spec/system/visit_user_journey_view_certificate_page_spec.rb
+++ spec/system/visit_user_journey_view_certificate_page_spec.rb
@@ -43,6 +43,7 @@ RSpec.describe 'View certificate page', type: :system do
       click_link 'Replace certificate'
       expect(current_path).to eql before_you_start_path(sp_component.component_type, sp_component.id, sp_component.encryption_certificate_id)
     end
+    
     it 'signing certificate information and navigates to next page' do
       certificate = create(:sp_signing_certificate)
       sp_component = certificate.component
@@ -51,5 +52,33 @@ RSpec.describe 'View certificate page', type: :system do
       click_link 'Add new certificate'
       expect(current_path).to eql before_you_start_path(sp_component.component_type, sp_component.id, sp_component.signing_certificates[0])
     end
+  end
+
+  it 'shows specific information when certificate is primary' do
+    first_certificate = create(:msa_signing_certificate)
+    defaults = {
+      usage: CERTIFICATE_USAGE::SIGNING,
+      value: second_certificate,
+      component: first_certificate.component
+    }
+    second_cert = Certificate.create(defaults)
+    visit root_path
+    click_link 'Signing certificate (primary)'
+    expect(page).to have_content 'GOV.UK Verify is adding your certificate to its configuration'
+    expect(page).to_not have_button('Add new certificate')
+  end
+
+  it 'shows specific information when certificate is secondary' do
+    first_certificate = create(:msa_signing_certificate)
+    defaults = {
+      usage: CERTIFICATE_USAGE::SIGNING,
+      value: second_certificate,
+      component: first_certificate.component
+    }
+    second_cert = Certificate.create(defaults)
+    visit root_path
+    click_link 'Signing certificate (secondary)'
+    expect(page).to have_content 'Wait for an email from GOV.UK Verify confirming your new signing certificate is in use'
+    expect(page).to_not have_button('Add new certificate')
   end
 end
