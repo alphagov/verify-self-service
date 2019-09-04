e39e895f0c206908d86009fe73ee4b66b61c84d9
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2019-09-04 15:20:27 +0100

    MdAKXfZX: Refactor and localise
    Created a method which returns true for primary or secondary signing certs
    Localise all new static text

--- spec/system/visit_index_page_spec.rb
+++ spec/system/visit_index_page_spec.rb
@@ -5,8 +5,6 @@ RSpec.describe 'IndexPage', type: :system do
     login_gds_user
   end
 
-  let(:second_certificate) { PKI.new.generate_encoded_cert(expires_in: 9.months) }
-
   it 'shows greeting without JS' do
     visit '/'
     expect(page).to have_content 'Manage certificates'
@@ -19,12 +17,7 @@ RSpec.describe 'IndexPage', type: :system do
 
   it 'shows whether certificate is primary or secondary when there are two signing certificates' do
     first_certificate = create(:msa_signing_certificate)
-    defaults = {
-      usage: CERTIFICATE_USAGE::SIGNING,
-      value: second_certificate,
-      component: first_certificate.component
-    }
-    Certificate.create(defaults)
+    create(:msa_signing_certificate, component: first_certificate.component)
     visit root_path
     expect(page).to have_content 'Signing certificate (primary)'
     expect(page).to have_content 'Signing certificate (secondary)'
