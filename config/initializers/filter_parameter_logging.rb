886d359b66db22dea87713064447950c5703e7d4
brenetic <b@brenetic.com>
2019-09-06 14:34:56 +0100

    Filter sensitive parameters from logs
    We do not want these parameters to be logged anywhere.

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,16 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [
+  :access_token,
+  :code,
+  :email,
+  :family_name,
+  :full_name,
+  :given_name,
+  :new_password,
+  :password,
+  :phone_number,
+  :temporary_password,
+  :totp_code
+]
