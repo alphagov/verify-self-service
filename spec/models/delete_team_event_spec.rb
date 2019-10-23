0d1adc50e4509e3d6fda7f0b1432680eb7cabbd7
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2019-10-23 16:34:17 +0100

    BAU: Prevent deleting GDS team
    GDS team/group is an admin team and should never be deleted.

--- spec/models/delete_team_event_spec.rb
+++ spec/models/delete_team_event_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe DeleteTeamEvent, type: :model do
       expect(delete_team_event).to eq DeleteTeamEvent.last
     end
 
-    it 'team is delete even if Cognito group does not exist or has already been deleted' do
+    it 'team is deleted even if Cognito group does not exist or has already been deleted' do
       stub_cognito_response(method: :delete_group, payload: 'ResourceNotFoundException')
       stub_cognito_response(method: :list_users_in_group, payload: [])
       delete_team_event = create(:delete_team_event)
@@ -38,6 +38,15 @@ RSpec.describe DeleteTeamEvent, type: :model do
       expect{ create(:delete_team_event, team: existing_team)}.to raise_error(ActiveRecord::RecordInvalid)
       expect(Team.exists?(existing_team.id)).to be true
     end
+
+    it 'when the team is GDS' do
+      stub_cognito_response(method: :delete_group)
+      stub_cognito_response(method: :list_users_in_group, payload: [])
+      gds_team = create(:team, team_alias: TEAMS::GDS)
+      expect{create(:delete_team_event, team: gds_team)}.to raise_error(ActiveRecord::RecordInvalid)
+      expect(Team.exists?(gds_team.id)).to be true
+    end
+
     it 'when cognito call fails' do
       stub_cognito_response(method: :delete_group, payload: 'ServiceError')
       stub_cognito_response(method: :list_users_in_group, payload: [])
