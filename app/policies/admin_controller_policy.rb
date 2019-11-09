7052b08344006460315547a00b5c39baafaa9dc1
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2019-11-09 17:39:13 +0000

    BAU: Add the ability to trigger metadata publish manually
    As admins we should be able to publish latest metadata manually - in case of previous
    failures or any other reasons.

--- app/policies/admin_controller_policy.rb
+++ app/policies/admin_controller_policy.rb
@@ -2,4 +2,8 @@ class AdminControllerPolicy < ApplicationPolicy
   def index?
     user.permissions.admin_management
   end
+
+  def publish_metadata?
+    user.permissions.admin_management
+  end
 end
--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe AdminController, type: :controller do
   include AuthSupport
+  include StorageSupport
 
+  context 'GET #index' do
     it 'should render when GDS user' do
       gdsuser_stub_auth
       get :index
@@ -13,14 +15,34 @@ RSpec.describe AdminController, type: :controller do
     it 'should not render when user is certificate manager' do
       certmgr_stub_auth
       get :index
-      expect(flash[:warn]).to eq t('shared.errors.authorisation')    
+      expect(flash[:warn]).to eq t('shared.errors.authorisation')
       expect(subject).to_not render_template(:index)
     end
 
     it 'should not render when user is user manager' do
       usermgr_stub_auth
       get :index
-      expect(flash[:warn]).to eq t('shared.errors.authorisation')    
+      expect(flash[:warn]).to eq t('shared.errors.authorisation')
       expect(subject).to_not render_template(:index)
     end
+  end
+
+  context 'GET #publish_metadata' do
+    it 'should publish metadata for a given environment' do
+      gdsuser_stub_auth
+      PublishServicesMetadataEvent.delete_all
+      get :publish_metadata, params: { environment: 'staging'}
+      expect(PublishServicesMetadataEvent.all.count).to eq 1
+      expect(flash[:notice]).to be nil
+    end
+
+    it 'should display warning if publishing metadata fails' do
+      stub_storage_client_service_error
+      gdsuser_stub_auth
+      PublishServicesMetadataEvent.delete_all
+      get :publish_metadata, params: { environment: 'staging'}
+      expect(PublishServicesMetadataEvent.all.count).to eq 0
+      expect(flash[:notice]).to eq(t('certificates.errors.cannot_publish'))
+    end
+  end
 end
