65d3a69925909dd7ee05e8f5764e4035f165c190
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2019-11-25 12:57:29 +0000

    Replace the net/http with our new api class

--- app/models/healthcheck/hub_check.rb
+++ app/models/healthcheck/hub_check.rb
@@ -1,4 +1,4 @@
-require 'net/http'
+require 'api/hub_config_api'
 
 module Healthcheck
   class HubCheck
@@ -7,16 +7,9 @@ module Healthcheck
     end
 
     def status
-      url = URI.join(Rails.configuration.hub_config_host, 'service-status')
-      req = Net::HTTP::Get.new(url.to_s)
-      res = Net::HTTP.start(url.host, url.port,
-                            use_ssl: url.scheme == 'https',
-                            open_timeout: 3,
-                            read_timeout: 3) { |http|
-        http.request(req)
-      }
+      response = HUB_CONFIG_API.healthcheck
 
-      return OK if res.code == "200"
+      return OK if response.status == 200
     end
   end
 end
