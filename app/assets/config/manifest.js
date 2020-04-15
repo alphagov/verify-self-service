fcbf5985e593fb296e75632d74d5d78f85e1e844
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2020-04-15 11:02:05 +0100

    BAU: Update vulnerable dependencies
    This commit updates gems' version to fix:
    - GHSA-65cv-r6x7-79hv (moderate severity) in ActionView's XSS
    - GHSA-33vf-4xgg-9r58 (moderate severity) in Puma's HTTP response splitting
    - CVE-2020-7595 (moderate severity) in Nokogiri dependency causing infinite loop
    - GHSA-hrqr-hxpp-chr3 (low severity) in Rack session hijack

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,5 @@
-//= link_tree ../images
+//= link application.js
+//= link application.css
+//= link_tree ../../../node_modules/govuk-frontend/govuk/assets/images
+//= link_tree ../../../node_modules/govuk-frontend/govuk/assets/fonts
 //= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
