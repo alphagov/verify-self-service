cc92ecd1409b4ed39e54cabe6d001875f544d1a3
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2020-05-01 07:25:24 +0100

    HUB-515: lower number threads spawned by rufus

--- app/models/polling/scheduler.rb
+++ app/models/polling/scheduler.rb
@@ -3,11 +3,12 @@ module Polling
   class Scheduler
     attr_reader :rufus_scheduler, :job, :time, :opts
     DEFAULT_TIMEOUT = '100.0s'.freeze
-    DEFAULT_NUMBER_POLLS = 12
-
+    DEFAULT_NUMBER_POLLS = 8
+    # MAX_WORK_THREADS is set to a value substantially lower than db connection pool, so rufus is limited to creating only 2 connections from the pool
+    MAX_WORK_THREADS = 2
     def initialize(opts = { overlap: false, timeout: DEFAULT_TIMEOUT, times: DEFAULT_NUMBER_POLLS, frequency: Rails.configuration.scheduler_polling_interval })
       @opts = opts
-      @rufus_scheduler ||= Rufus::Scheduler.new(frequency: opts[:frequency], max_work_threads: ENV['DB_POOL'] || ENV['RAILS_MAX_THREADS'] || DEFAULT_NUMBER_POLLS)
+      @rufus_scheduler ||= Rufus::Scheduler.new(frequency: opts[:frequency], max_work_threads: MAX_WORK_THREADS)
       @rufus_scheduler.stderr = StringIO.new
     end
 
@@ -19,6 +20,8 @@ module Polling
     end
 
     def perform(action = -> {})
+      #rufus starts a new thread for every job, active record does not share connection between threads
+      # below allows the connection in the rufus thread to be released back to the pool when the thread is done
       ActiveRecord::Base.connection_pool.with_connection do
         @job = @rufus_scheduler.method("schedule_#{@mode}")
                               .call(time, **opts, &action)
