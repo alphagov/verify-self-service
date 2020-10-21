d4c3bf0bdc6b17f851fea3bf5781c69d102b2fc7
Jamie Maynard <james.maynard@digital.cabinet-office.gov.uk>
2020-10-21 15:54:01 +0100

    HUB-733 Tell sentry to run in prod, staging & int
    Tell sentry to run in production, integration and staging and to get
    the current environment from from the environment variable SENTRY_ENV
    rather than from the rails/rack environment definition.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 Raven.configure do |config|
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.transport_failure_callback = ->(event) { Rails.logger.error(event.to_s) }
-  config.environments = %w[production]
-  #config.current_environment = ENV.fetch('SENTRY_ENV', 'unspecified')
+  config.environments = %w[ production staging integration ]
+  config.current_environment = ENV.fetch('SENTRY_ENV', 'unspecified')
 end
