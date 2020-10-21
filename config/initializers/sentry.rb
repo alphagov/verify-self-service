e557c6d423b1357dcf085d968bb0e4000969f073
Jamie Maynard <james.maynard@digital.cabinet-office.gov.uk>
2020-10-21 15:25:17 +0100

    HUB-733 Disable sentry environment reporting

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,5 +2,5 @@ Raven.configure do |config|
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.transport_failure_callback = ->(event) { Rails.logger.error(event.to_s) }
   config.environments = %w[production]
-  config.current_environment = ENV.fetch('SENTRY_ENV', 'unspecified')
+  #config.current_environment = ENV.fetch('SENTRY_ENV', 'unspecified')
 end
