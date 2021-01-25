7d5102ac7bd66e5e6c3017dfc7cd72abf149d9ad
Jamie Maynard <james.maynard@digital.cabinet-office.gov.uk>
2021-01-25 13:15:41 +0000

    BAU Update Startup to use Docker
    This should allow you to spin up and instance of Self Service using
    Docker.  It sorts out creating, populating and running the database as
    well as running verify self service.

--- startup.sh
+++ startup.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-set -e
-bundle check || bundle install
 
-yarn check || yarn install
-
-bin/rails s
+while true; do
+    read -p "Run using Docker? [y/n] " p
+    case $p in
+        y|Y|yes)    docker build . -f run.Dockerfile -t verify-self-service
+                    docker run --rm -p 8080:8080 -it verify-self-service
+                    break
+                    ;;
+        n|N|no)     set -e
+                    bundle check || bundle install
+                    yarn check || yarn install
+                    bin/rails s
+                    break
+                    ;;
+        x|X|exit)   exit 0
+                    ;;
+        *)          echo "Unknown option."
+                    ;;
+    esac
+done
