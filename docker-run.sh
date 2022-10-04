cb648308663d25ab83eef157b34ab6f48c1ebdc7
James Mclearie <james.mclearie@digital.cabinet-office.gov.uk>
2022-10-04 08:51:33 +0100

    Update

--- docker-run.sh
+++ docker-run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd $(dirname "${BASH_SOURCE[0]}")
+# cd $(dirname "${BASH_SOURCE[0]}")
 
 docker build -t verify-self-service:latest -f Dockerfile . 2>&1
 echo "verify-self-service:latest"
